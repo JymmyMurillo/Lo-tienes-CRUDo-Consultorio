b61c2c958d9153081299cb0e45cfe226d730ccfa
Jymmy Murillo <murillojymmy@gmail.com>
2022-03-28 14:39:08 -0500

    updating favicon and page name

--- view/citasView.php
+++ view/citasView.php
@@ -1,6 +1,6 @@
 <?php
 include_once './controler/cita.php';
-$resultado = Cita:: all(); 
+$resultado = Cita::all();
 //var_dump ($resultado);
 ?>
 
@@ -15,7 +15,8 @@ $resultado = Cita:: all();
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <!-- cdn icnonos-->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
-  <title>Date</title>
+  <link rel="shortcut icon" href="./img/php.jpg" type="image/ico" />
+  <title>Citas</title>
 </head>
 
 <body class="py-3 bg-dark d-flex justify-content-center align-items-center" style="min-height: 100vh;">
